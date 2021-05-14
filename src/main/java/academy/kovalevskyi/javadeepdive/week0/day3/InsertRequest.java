b5a2e2bab75d2c022f81e94fd0a2c7822261d0d1
seralekseenko <seralekseenko@ukr.net>
2021-05-14 16:42:03 +0300

    day3 InsertRequest.java done

--- src/main/java/academy/kovalevskyi/javadeepdive/week0/day3/InsertRequest.java
+++ src/main/java/academy/kovalevskyi/javadeepdive/week0/day3/InsertRequest.java
@@ -1,6 +1,9 @@
 package academy.kovalevskyi.javadeepdive.week0.day3;
 
 import academy.kovalevskyi.javadeepdive.week0.day2.Csv;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /** Вставляет данные в таблицу. Таблица может быть пустой или непустой.
  */
@@ -8,15 +11,19 @@ public class InsertRequest extends AbstractRequest<Csv> {
 
   private final String[] line;
 
-  private InsertRequest(Csv target, String[] line) {
+  private InsertRequest(Csv target, String[] line) throws RequestException {
     super(target);
     this.line = line;
   }
 
   @Override
   protected Csv execute() throws RequestException {
-    // TODO
-    return null;
+    var resultBuilder = new Csv.Builder().header(target.header());
+
+    ArrayList<String[]> values = new ArrayList<>(Arrays.asList(target.values()));
+    values.add(line);
+    resultBuilder.values(values.toArray(String[][]::new));
+    return resultBuilder.build();
   }
 
   public static class Builder {
@@ -25,16 +32,28 @@ public class InsertRequest extends AbstractRequest<Csv> {
     private String[] line;
 
     public Builder insert(String[] line) {
+      if (line == null) {
+        throw new NullPointerException("New line cant be empty!");
+      }
       this.line = line;
       return this;
     }
 
     public Builder to(Csv target) {
+      if (target == null) {
+        throw new NullPointerException("You cant insert anything in null!");
+      }
       this.target = target;
       return this;
     }
 
-    public InsertRequest build() {
+    public InsertRequest build() throws RequestException {
+      if (line.length != target.header().length) {
+        throw new RequestException(String.format("The new line is different in length from the "
+            + "header! Header length: %d, New line's length: %d",
+            target.header().length,
+            line.length));
+      }
       return new InsertRequest(target, line);
     }
   }
---
+++ src/test/java/academy/kovalevskyi/javadeepdive/week0/day3/InsertRequestTest.java
@@ -0,0 +1,18 @@
+package academy.kovalevskyi.javadeepdive.week0.day3;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Test;
+
+class InsertRequestTest extends AbstractRequestTest {
+
+  @Test
+  public void insert_nothing() {
+
+  }
+
+  @Test
+  public void insert_something() {
+
+  }
+}
