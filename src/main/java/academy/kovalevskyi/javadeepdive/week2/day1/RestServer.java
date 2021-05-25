12d77e3eb544aad56f8aca8ae8b2e3c76dcc9656
seralekseenko <seralekseenko@ukr.net>
2021-05-25 22:06:42 +0300

    init w2 d1

---
+++ src/main/java/academy/kovalevskyi/javadeepdive/week2/day1/RestServer.java
@@ -0,0 +1,27 @@
+package academy.kovalevskyi.javadeepdive.week2.day1;
+
+import academy.kovalevskyi.javadeepdive.week1.day2.HttpMethod;
+import org.reflections.Reflections;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.ServerSocket;
+import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class RestServer extends Thread {
+  // TODO
+  public RestServer(String packagePath) {
+    // TODO
+  }
+  public void run() {
+    // TODO
+  }
+  public void stopServer() {
+    // TODO
+  }
+  public boolean isLive() {
+    // TODO
+  }
+}
